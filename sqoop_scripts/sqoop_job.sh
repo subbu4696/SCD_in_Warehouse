3dcc0722c38b846dd13cf050b596dfe28922df2d
subbu_4696 <subhashvarma4696@gmail.com>
2022-03-26 19:40:35 +0530

    commit n

--- sqoop_scripts/sqoop_job.sh
+++ sqoop_scripts/sqoop_job.sh
@@ -13,7 +13,7 @@ sqoop job --delete loadcustomers
 
 sqoop-job --create loadcustomers -- import  \
 --connect jdbc:mysql://localhost:${PORT_NO}/${DB_SQL}?useSSL=False \
---username root --password-file file:///home/saif/SCD_in_Warehouse/env/sql.pwd \
+--username root --password-file ${PASWD_PATH} \
 --table v_customer  --as-parquetfile \
 --target-dir ${HADOOP_FILEPATH}/stores/stage/customers  --append \
 --split-by CustomerID --num-mappers 1 --check-column ModifiedDate --incremental lastmodified \
@@ -25,7 +25,7 @@ sqoop job --delete loadsalesorderheader
 
 sqoop-job --create loadsalesorderheader -- import  \
 --connect jdbc:mysql://localhost:${PORT_NO}/${DB_SQL}?useSSL=False \
---username root --password-file file:///home/saif/SCD_in_Warehouse/env/sql.pwd \
+--username root --password-file ${PASWD_PATH} \
 --table v_salesorderheader --as-parquetfile \
 --target-dir ${HADOOP_FILEPATH}/stores/stage/salesorderheader  \
 --split-by SalesOrderID --num-mappers 1 --check-column SalesOrderID --incremental append 
@@ -39,7 +39,7 @@ sqoop job --delete loadsalesorderdetail
 
 sqoop-job --create loadsalesorderdetail -- import  \
 --connect jdbc:mysql://localhost:${PORT_NO}/${DB_SQL}?useSSL=False \
---username root --password-file file:///home/saif/SCD_in_Warehouse/env/sql.pwd \
+--username root --password-file ${PASWD_PATH} \
 --table v_salesorderdetails  --as-parquetfile \
 --target-dir ${HADOOP_FILEPATH}/stores/stage/salesorderdetails  \
 --split-by SalesOrderDetailID --num-mappers 1 --check-column SalesOrderDetailID --incremental append 
@@ -52,7 +52,7 @@ sqoop job --delete loadproducts
 
 sqoop-job --create loadproducts -- import  \
 --connect jdbc:mysql://localhost:${PORT_NO}/${DB_SQL}?useSSL=False \
---username root --password-file file:///home/saif/SCD_in_Warehouse/env/sql.pwd \
+--username root --password-file ${PASWD_PATH} \
 --table v_product  --as-parquetfile \
 --target-dir ${HADOOP_FILEPATH}/stores/stage/products  \
 --split-by productId --num-mappers 1 --check-column modifieddate --incremental lastmodified 
@@ -64,7 +64,7 @@ sqoop job --delete loadcreditcard
 
 sqoop job --create loadcreditcard -- import \
 --connect jdbc:mysql://localhost:${PORT_NO}/${DB_SQL}?useSSL=False \
---username root --password-file file:///home/saif/SCD_in_Warehouse/env/sql.pwd \
+--username root --password-file ${PASWD_PATH} \
 --table creditcard --as-parquetfile \
 --target-dir ${HADOOP_FILEPATH}/stores/stage/creditcard --num-mappers 1 --check-column ModifiedDate \
 --append --incremental lastmodified 
